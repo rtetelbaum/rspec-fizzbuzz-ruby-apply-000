62d7a93d44b5ad4cfc6231ad1423ae098165070e
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-07 21:23:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,19 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-puts "Pick a number."
-
-int = gets.strip.to_i
-
 #fizzbuzz
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
-    elsif int % 5 == 0
-    puts "Buzz"
+  if int % 3 == 0 && int % 15 != 0
+    "Fizz"
+    elsif int % 5 == 0 && int % 15 != 0
+    "Buzz"
     elsif int % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   end
 end
-
-fizzbuzz(int)
